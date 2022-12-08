1e3e5dc146044458e1a0a227ddd317ab884fd7cc
AlternateForce <43939142+AlternateForce@users.noreply.github.com>
2022-12-07 21:05:33 -0600

    File opener

--- study.py
+++ study.py
@@ -3,9 +3,19 @@ termList = []
 termDef = []
 number = -1
 while(term != 'Stop'):
-    print('Please Enter a term or "Stop"')
+    print('Please Enter a term, "Stop", or "File"')
     term = input().capitalize()
     if term == 'Stop': break
+    if term == 'File':
+        print('Please enter the file name:')
+        f = open(input(), 'r')
+        line = f.readline()
+        while (line):
+            termList.append(line[6:-1 ])
+            line = f.readline()
+            termDef.append(line[12:-1])
+            line = f.readline()
+        continue
     termList.append(term)
     print('Please enter the definition for: ' + term)
     termDef.append(input())
